ecc780deae9c0a20481d5a8ae6e45250b0dfaf64
Alexei Shirokov <temp_555@mail.ru>
2025-03-22 22:32:38 +0000

    Этап 3. Создание DVC-пайплайна

---
+++ part2_dvc/scripts/data.py
@@ -0,0 +1,46 @@
+# scripts/data.py
+
+# 1 — импорты
+import pandas as pd
+from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv
+import yaml
+
+# 2 — вспомогательные функции
+def create_connection():
+
+    # Загружаем переменные окружения из .env, который находится на два уровня выше
+    # dotenv_path = os.path.join(os.path.dirname(__file__), '../../.env')
+    load_dotenv()
+    host = os.environ.get('DB_DESTINATION_HOST')
+    port = os.environ.get('DB_DESTINATION_PORT')
+    db = os.environ.get('DB_DESTINATION_NAME')
+    username = os.environ.get('DB_DESTINATION_USER')
+    password = os.environ.get('DB_DESTINATION_PASSWORD')
+    
+    print(f'postgresql://{username}:{password}@{host}:{port}/{db}')
+    conn = create_engine(f'postgresql://{username}:{password}@{host}:{port}/{db}', connect_args={'sslmode':'require'})
+    return conn
+
+# 3 — главная функция
+def get_data():
+    # 3.1 — загрузка гиперпараметров
+    with open('params.yaml', 'r') as fd:
+        params = yaml.safe_load(fd)
+    # 3.2 — загрузки предыдущих результатов нет, так как это первый шаг
+
+    # 3.3 — основная логика
+    conn = create_connection()
+    data = pd.read_sql('''SELECT * 
+                          FROM real_estate_prices_clean''', 
+                       conn) 
+    conn.dispose()
+
+    # 3.4 — сохранение результата шага
+    os.makedirs('data', exist_ok=True)
+    data.to_csv('data/initial_data.csv', index=False)
+
+# 4 — защищённый вызов главной функции
+if __name__ == '__main__':
+    get_data()
---
+++ part2_dvc/scripts/fit.py
@@ -0,0 +1,71 @@
+# scripts/fit.py
+
+import numpy as np
+import pandas as pd
+# from sklearn.compose import ColumnTransformer
+# from sklearn.pipeline import Pipeline
+# from category_encoders import CatBoostEncoder
+# from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.model_selection import cross_validate
+from catboost import CatBoostRegressor
+import yaml
+import os
+import json
+import joblib
+
+# обучение модели
+def evaluate_and_fit_model():
+	# Прочитайте файл с гиперпараметрами params.yaml
+    with open('params.yaml', 'r') as fd:
+        params = yaml.safe_load(fd)
+	
+    # Загрузите результат предыдущего шага: inital_data.csv
+    data = pd.read_csv('data/initial_data.csv')
+    data = data.drop(['id', 'building_id', # точно не повлияет на результат прогнозирования
+                      'latitude', 'longitude', # могут "запутать модель"
+                      'floor', 'flats_count','floors_total'], axis=1) # скорее всего, не значимы
+
+    # Определение признаков и целевой переменной
+    X = data.drop('price', axis=1)
+    y = data['price']
+   
+    # Определение категориальных и числовых признаков
+    categorical_features = X.select_dtypes(include=['object', 'boolean']).columns.tolist()
+    categorical_features.append('building_type_int')
+
+    numerical_features = X.select_dtypes(include=[np.number]).columns.tolist()
+    numerical_features.remove('building_type_int')
+
+    # Настройка модели CatBoost
+    model = CatBoostRegressor(iterations=params['iterations'], 
+                              learning_rate=params['learning_rate'], 
+                              depth=params['depth'],
+                              cat_features=categorical_features,
+                              verbose=params['verbose'],
+                              random_state=555)
+
+    # Кросс-валидация
+    cv_res = cross_validate(model, 
+                            X, y, 
+                            cv=params['n_splits'], 
+                            scoring=params['metrics'],
+                            n_jobs=params['n_jobs'], 
+                            error_score=params['error_score'])
+
+    # сохранение результата кросс-валидации в JSON
+    cv_res_dict = dict()
+    for key, value in cv_res.items():
+        cv_res_dict[f'{key}'] = value.mean().round(2)
+    os.makedirs('cv_results', exist_ok=True)
+    with open('cv_results/cv_res.json', "w") as f:
+        json.dump(cv_res_dict, f, indent=4)  # indent=4 для красивого форматирования
+
+    # Обучение модели на ВСЕХ данных ПОСЛЕ кросс-валидации
+    model.fit(X, y)
+    # сохранение модели в файл
+    os.makedirs('models', exist_ok=True)
+    with open('models/fitted_model.pkl', 'wb') as fd:
+        joblib.dump(model, fd)
+
+if __name__ == '__main__':
+	evaluate_and_fit_model()
